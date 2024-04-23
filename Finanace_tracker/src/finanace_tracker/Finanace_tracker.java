0cb0baa00c49e29c7138e7b19095ec3a0744c403
HasinduNimesh <123946580+HasinduNimesh@users.noreply.github.com>
2024-04-23 15:08:51 +0530

    New GUI

---
+++ Finanace_tracker/src/finanace_tracker/Finanace_tracker.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finanace_tracker;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Finanace_tracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
