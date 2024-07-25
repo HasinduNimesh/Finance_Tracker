4b65cb3245eb2b61e68e51727865a98981c28972
HasinduNimesh <123946580+HasinduNimesh@users.noreply.github.com>
2024-07-25 14:36:38 +0530

    new_v6

---
+++ Finanace_tracker/src/loginandsignup/database/DatabaseConfig.java
@@ -0,0 +1,38 @@
+package loginandsignup.database;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import loginandsignup.UserAuthentication;
+
+public class DatabaseConfig {
+
+ /*   "CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+);" */
+public static void main(String[] args) {
+   // String jdbcURL = "jdbc:h2:.\\src\\loginandsignup\\database\\UserAuth";
+   // String USER = "sa";
+    //String PASSWORD = "1234";
+
+    try {
+      //  Connection connection= DriverManager.getConnection(jdbcURL, USER, PASSWORD);
+       // System.out.println("Connected to H2 database");
+    //done UserAuthentication.registerUser("Admin1", "StrongPassword123", "admin@kdu.ac.lk");
+        UserAuthentication.authenticateUser("Admin1", "StrongPassword123");
+
+       // connection.close();
+
+    } catch (SQLException | NoSuchAlgorithmException e) {
+        e.printStackTrace();
+    }
+
+
+}
+}
+ 
